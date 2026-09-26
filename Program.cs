df8b916c8b9619f9523f6858b8982124b2d89efa
Elizaveta _0_0_ <rebcovetcelizaveta@gmail.com>
2026-09-26 16:26:48 +0300

    delite icons

--- Program.cs
+++ Program.cs
@@ -11,12 +11,6 @@ builder.Services.AddHttpClient();
 var app = builder.Build();
 app.UseCors("all");
 
-var provider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
-app.UseStaticFiles(new StaticFileOptions
-{
-    FileProvider = provider
-});
-
 app.MapGet("/", () => Results.Content(File.ReadAllText("index.html"), "text/html"));
 
 app.MapGet("/api/students", () =>
